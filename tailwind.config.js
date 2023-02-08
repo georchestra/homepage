a6bbc2eac38e1b8cd67995e8eb2f5f89fa0e2d0b
Florent gravin <florent.gravin@camptocamp.com>
2023-02-08 12:20:45 +0100

    chore: add tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgb(133,18,126)',
+        secondary: '#1b1f3b',
+      },
+    },
+  },
+  plugins: [],
+}
